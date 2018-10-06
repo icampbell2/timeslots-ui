fb97fd27e173bceb7206cc8d270e9bd50810f7b9
Ian Campbell <icampbell2@github.com>
2018-10-06 15:18:33 -0400

    footer copyright text and styling

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,13 +1,19 @@
+import AppBar from '@material-ui/core/AppBar';
 import * as React from 'react';
 import './Footer.css';
 
 class Footer extends React.Component {
   public render() {
     return (
-      <footer id="footer">
-        <h1 id="footer-title">Welcome to React</h1>
-      </footer>
+      <AppBar position="static">
+        <footer id="footer">
+          <h1 id="footer-title">&copy; {this.getYear()} Ian Campbell</h1>
+        </footer>
+      </AppBar>
     );
+  }
+  protected getYear(): number {
+    return new Date().getFullYear();
   }
 }
 
