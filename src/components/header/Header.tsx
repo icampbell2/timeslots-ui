db71265d83b6f465c5ed56fe88985241aae87f7a
Ian Campbell <icampbell2@github.com>
2018-10-06 16:09:03 -0400

    header styling and navbar

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,26 @@
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Toolbar from '@material-ui/core/Toolbar';
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 class Header extends React.Component {
   public render() {
     return (
-      <header id="header">
-        <h1 id="header-title">Welcome to React</h1>
-      </header>
+      <AppBar position="static" title="Time Slots">
+        <Toolbar>
+        <header id="header">
+          <span id="header-title">Time Slots</span>
+          <Link to="/home">
+            <Button>Home</Button>
+          </Link>
+          <Link to="/about">
+            <Button>About</Button>
+          </Link>
+        </header>
+        </Toolbar>
+      </AppBar>
     );
   }
 }
