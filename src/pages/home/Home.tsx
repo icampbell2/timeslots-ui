350fdc74d8578b0756d0d8dbf355f7ce0edf11ce
Ian Campbell <icampbell2@github.com>
2018-10-06 13:47:40 -0400

    new Home and About pages

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './Home.css';
+
+class Home extends React.Component {
+  public render() {
+    return (
+      <p className="home">
+        Home
+      </p>
+    );
+  }
+}
+
+export default Home;
