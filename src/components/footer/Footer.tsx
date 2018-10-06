b6053e4841d23ab41f55ebba2ab14d2a4f751a4c
Ian Campbell <icampbell2@github.com>
2018-10-06 13:47:11 -0400

    new Footer and Header components

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './Footer.css';
+
+class Footer extends React.Component {
+  public render() {
+    return (
+      <footer id="footer">
+        <h1 id="footer-title">Welcome to React</h1>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
